eaed8e9830a08780394d58e635085f671d3993d5
Kaley <kaleyrennie14@gmail.com>
2020-02-14 01:27:37 -0800

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accouting Systems");
+            Console.WriteLine("Remember, we're counting on you!");
+            Console.Read();
+
+        }
+    }
+}
